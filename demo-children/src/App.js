8d14d8e7b7fa383f2bbd08304640a165610562af
pham van thinh <phamvanthinh2001x@gmail.com>
2020-12-04 12:21:45 +0700

    create prop children

---
+++ demo-children/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Accordion from './components/Accordion'
+
+function App() {
+  return (
+    <div className="App">
+     <Accordion heading="Heading" content="Abc Xyz"/>
+    </div>
+  );
+}
+
+export default App;
