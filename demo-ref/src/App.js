227eba3529af88581c2dd5bb091402f1e1d85f6e
pham van thinh <phamvanthinh2001x@gmail.com>
2020-12-04 16:42:21 +0700

    focus

---
+++ demo-ref/src/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.inputElement = React.createRef();
+    this.inputElement.current.focus();
+  }
+  componentDidMount(){
+    this.inputElement.current.focus();
+  }
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <input type="text" ref={this.inputElement}/>
+    </div>
+  );
+}
+
+export default App;
